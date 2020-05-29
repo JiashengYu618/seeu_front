b065999c204bc909edfe0fc4235a9f3c00090f50
Jiasheng Yu <JiashengYu618@gmail.com>
2020-05-29 19:06:34 +0800

    final

--- seeu/src/components/videos-list/videos-list.vue
+++ seeu/src/components/videos-list/videos-list.vue
@@ -2,7 +2,11 @@
   <div class="video-list">
     <swiper class="swiper" :options="swiperOption">
       <swiper-slide>
-        <video-player class="video-player-box" ref="videoPlayer" :options="playerOptions"></video-player>
+        <video-player class="video-player-box vjs-custom-skin" ref="videoPlayer" :options="playerOptions"></video-player>
+        <Description></Description>
+      </swiper-slide>
+      <swiper-slide>
+        <video-player class="video-player-box" ref="videoPlayer" :options="playerOptions2"></video-player>
         <Description></Description>
       </swiper-slide>
     </swiper>
@@ -11,21 +15,44 @@
 
 <script>
 import Description from "@/components/home/description";
+import { swiper, swiperSlide } from 'vue-awesome-swiper' 
 export default {
   name: "VideoList",
   components: {
-    Description
+    Description,
+    
   },
   data() {
     return {
       swiperOption: {
         direction: "vertical",
+        autoHeight: true, //自动高度。设置为true时，wrapper和container会随着当前slide的高度而发生变化       
+        height: window.innerHeight, // 高度设置，占满设备高度
         pagination: {
           el: ".swiper-pagination",
-          clickable: true
-        }
+          clickable: true,            
+        },
+        
       },
       playerOptions: {
+        // videojs options
+        height: window.innerHeight,
+        width: window.innerWidth,
+        autoplay: true,
+        muted: false,
+        loop: true,
+        preload:'auto',
+        language: "en",
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        sources: [
+          {
+            type: "video/mp4",
+            src: "http://localhost:8181/3.mp4"
+          }
+        ]
+      },
+      
+        playerOptions2: {
         // videojs options
         height: window.innerHeight,
         width: window.innerWidth,
@@ -37,12 +64,13 @@ export default {
         sources: [
           {
             type: "video/mp4",
-            src: "http://localhost:8181/3.mp4"
+            src: "http://localhost:8181/2.mp4"
           }
         ]
       }
     };
-  }
+  },
+  
 };
 </script>
 
@@ -72,4 +100,6 @@ export default {
     }
   }
 }
+
+
 </style>
